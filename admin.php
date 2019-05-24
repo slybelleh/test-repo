738e2c034f401841173808faf6e35f303991b091
slybelleh <44547823+slybelleh@users.noreply.github.com>
2019-05-24 10:07:06 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,49 @@
+<html>
+    
+       <div class="button">
+          
+           
+    </div> 
+        
+<head>UNIVERSITY OF KABIANGA DISPENSARY</head>
+    <br>
+<body>
+    <img src="logo.png" width="100" height="80" border="1" align="center" title="logo"/>
+    <embed src="sbk.mp4" width="50%" height="30%"> 
+    <style>
+         body { background-color:peachpuff;
+             font-family:sans-serif;
+             background-position:center;
+             font-size: 35;
+             border: 1;
+         }
+          ul{
+              float: left;
+              list-style-type: none;
+          }
+        ul li{
+            display: inline-block;
+          }
+         </style>
+   
+<header>
+       
+        <div class="main">
+    
+             <ul>
+    <li><a href="admin.php">Home</a></li>
+     <li><a href="admin.html">Services</a></li>
+     <li><a href="healthlogin.php">log in</a></li>
+    </ul></div>
+    <br>
+    
+    <br>
+    <br>
+    <p>welcome to University of kabainga dispensary site. we are here to serve you all time. for more queries, do not hesitate to contact us at uokhealth@gmail.com </p>
+</header>
+    
+     
+<embed>
+
+    </body>
+    </html>
